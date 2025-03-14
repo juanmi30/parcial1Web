623607270751f19a9117fdf258e64a0d5a651e29
Juan Miguel Manrique <j.manriquec@uniandes.edu.co>
2025-03-14 09:14:18 -0500

    final 1-3

--- my-app/src/components/RobotDetail.js
+++ my-app/src/components/RobotDetail.js
@@ -1,22 +1,30 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Container } from "react-bootstrap";
 
 function RobotDetail({ robot, onClose }) {
     if (!robot) return null;
 
     return (
-        <Card className="mt-3">
-            <Card.Img variant="top" src={robot.imagen} alt={robot.nombre} />
-            <Card.Body>
-                <Card.Title>{robot.nombre}</Card.Title>
-                <Card.Text>
-                    <strong>Año de Fabricación:</strong> {robot.añoFabricacion} <br />
-                    <strong>Capacidad de Procesamiento:</strong> {robot.capacidadProcesamiento} <br />
-                    <strong>Humor:</strong> {robot.humor}
-                </Card.Text>
-                <Button variant="danger" onClick={onClose}>Cerrar</Button>
-            </Card.Body>
-        </Card>
+
+        <Container className="mt-3">
+            <Card className="robot-detail-card" style={{ width: "15rem" }}>
+                <Card.Body>
+                    <Card.Img
+                        variant="top"
+                        src={robot.imagen}
+                        alt={robot.nombre}
+                        className="robot-img"
+                    />
+                    <Card.Title className="text-center">{robot.nombre}</Card.Title>
+                    <Card.Text>
+                        <strong>→ Año de Fabricación:</strong> {robot.añoFabricacion} <br />
+                        <strong>→ Capacidad de Procesamiento:</strong> {robot.capacidadProcesamiento} <br />
+                        <strong>→ Humor:</strong> {robot.humor}
+                    </Card.Text>
+                    <Button variant="danger" onClick={onClose}>Cerrar</Button>
+                </Card.Body>
+            </Card>
+        </Container>
     );
 }
 
